d5e133ff49cd8c91a6a1ca65e4c629f0cd935179
andresvia <andres.via@gmail.com>
2016-08-11 15:07:23 -0500

    imports con full path y fmts

--- routers/router.go
+++ routers/router.go
@@ -1,18 +1,18 @@
 package routers
 
 import (
-	"rulerClient/controllers"
+	"github.com/apdaza/rulerClient/controllers"
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
-		beego.Router("/listarDominios", &controllers.DominiosController{})
-		beego.Router("/agregarDominios", &controllers.AddDominiosController{})
-		beego.Router("/editarDominio/:id:int", &controllers.EditDominiosController{})
-        beego.Router("/eliminarDominio/:id:int", &controllers.DeleteDominiosController{})
-		beego.Router("/listarPredicados", &controllers.PredicadosController{})
-		beego.Router("/agregarPredicados", &controllers.AddPredicadosController{})
-		beego.Router("/editarPredicado/:id:int", &controllers.EditPredicadosController{})
-        beego.Router("/eliminarPredicado/:id:int", &controllers.DeletePredicadosController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/listarDominios", &controllers.DominiosController{})
+	beego.Router("/agregarDominios", &controllers.AddDominiosController{})
+	beego.Router("/editarDominio/:id:int", &controllers.EditDominiosController{})
+	beego.Router("/eliminarDominio/:id:int", &controllers.DeleteDominiosController{})
+	beego.Router("/listarPredicados", &controllers.PredicadosController{})
+	beego.Router("/agregarPredicados", &controllers.AddPredicadosController{})
+	beego.Router("/editarPredicado/:id:int", &controllers.EditPredicadosController{})
+	beego.Router("/eliminarPredicado/:id:int", &controllers.DeletePredicadosController{})
 }
